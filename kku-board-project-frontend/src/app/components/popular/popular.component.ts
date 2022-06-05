88c6a7cdc880569545b81ae760a35933e5059fef
sasithon17 <sasithon.ko@kkumail.com>
2022-06-05 21:26:03 +0700

    add popular component

---
+++ kku-board-project-frontend/src/app/components/popular/popular.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit ,ViewChild} from '@angular/core';
+
+@Component({
+  selector: 'popular' ,
+  templateUrl: './popular.component.html',
+  styleUrls: ['./popular.component.css'],
+})
+export class PopularComponent implements OnInit {
+
+  // @Input() role = ""
+
+  dataBase = [
+    {
+      eventHeader: "รับน้องใหม่2565",
+      description: "ปฐมนิเทศนักศึกษาใหม่และพิธีบายศรีสู่ขวัญนักศึกษาใหม่",
+      attendees: 99,
+      eventType: "onsite",
+      location: "ณ หอกาญจนาภิเษก",
+      startDate: 1654275600,
+      endDate: 1654362000,
+      startTime: 1654327541,
+      endTime: 1654328541,
+      roleAccept: ["KKU59"],
+      image: "https://campus.campus-star.com/app/uploads/2019/07/KKU01-768x511.jpg",
+      eventId: String,
+      clubName: String,
+    }
+  ];
+
+  constructor() {}
+  ngOnInit() {}
+}
+
