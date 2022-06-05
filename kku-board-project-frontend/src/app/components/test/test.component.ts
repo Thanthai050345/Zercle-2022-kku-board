72c3d901e72516cdb90dac1f67067379582c2925
sasithon17 <sasithon.ko@kkumail.com>
2022-06-05 21:36:22 +0700

    add popular component

--- kku-board-project-frontend/src/app/components/test/test.component.ts
+++ kku-board-project-frontend/src/app/components/test/test.component.ts
@@ -1,12 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewChild} from '@angular/core';
+
+import { NzDatePickerComponent } from 'ng-zorro-antd/date-picker';
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
   styleUrls: ['./test.component.css'],
 })
+
+
 export class TestComponent implements OnInit {
-  
   constructor() {}
-  
+
   ngOnInit(): void {}
+
+  startValue: Date | null = null;
+  endValue: Date | null = null;
+  @ViewChild('endDatePicker') endDatePicker!: NzDatePickerComponent;
+
+  disabledStartDate = (startValue: Date): boolean => {
+    if (!startValue || !this.endValue) {
+      return false;
+    }
+    return startValue.getTime() > this.endValue.getTime();
+  };
+
+  disabledEndDate = (endValue: Date): boolean => {
+    if (!endValue || !this.startValue) {
+      return false;
+    }
+    return endValue.getTime() <= this.startValue.getTime();
+  };
+
+  handleStartOpenChange(open: boolean): void {
+    if (!open) {
+      this.endDatePicker.open();
+    }
+    console.log('handleStartOpenChange', open);
+  }
+
+  handleEndOpenChange(open: boolean): void {
+    console.log('handleEndOpenChange', open);
+  }
 }
+
+
