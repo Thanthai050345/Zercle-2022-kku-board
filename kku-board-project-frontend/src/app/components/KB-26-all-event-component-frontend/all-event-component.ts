8688e52273bfb99210fc67b1f5f6667e6dcc6675
amonrit <76424455+amonrit@users.noreply.github.com>
2022-06-06 17:11:28 +0700

    KB-26 <message>

--- kku-board-project-frontend/src/app/components/KB-26-all-event-component-frontend/all-event-component.ts
+++ kku-board-project-frontend/src/app/components/KB-26-all-event-component-frontend/all-event-component.ts
@@ -1,4 +1,8 @@
 import { Component,Input ,OnInit } from '@angular/core';
+import * as dayjs from 'dayjs'
+import{ Time } from 'src/app/interfaces/time'
+import "dayjs/locale/th";
+
 @Component({
   selector: 'all-event',
   templateUrl: './all-event-component.html',
@@ -9,8 +13,8 @@ export class AllEventComponent implements OnInit {
   @Input() role = ""
   dataBase = [
     {
-      eventHeader: "ทอดผ้าป่า",
-      description: "มข.เข้าร่วมพิธีทอดผ้าป่าสนับสนุนโครงการทุนเล่าเรียนหลวงฯ",
+      eventHeader: "มข.เข้าร่วมพิธีทอดผ้าป่าสนับสนุนโครงการทุนเล่าเรียนหลวงฯ",
+      description: "ในสมัยพุทธกาล เมื่อพระผู้มีพระภาคเจ้าประทับ ณ พระเชตวนาราม ซึ่งเป็นพระอารามที่อนาถบิณฑิกเศรษฐีได้สร้างถวายเป็นพุทธนิวาส ได้มีภิกษุ ๓๐ รูป ชาวเมืองปาฐา ซึ่งอยู่ด้านทิศตะวันตกในแคว้นโกศลเดินทางมาหมายจะเฝ้าพระพุทธองค์ที่เมืองสาวัตถี แต่มาไม่ทันเพราะใกล้ถึงวัน",
       attendees: 99,
       eventType: "onsite",
       location: "ณ วัดธาตุ พระอารามหลวงขอนแก่น",
@@ -20,10 +24,34 @@ export class AllEventComponent implements OnInit {
       endTime: 1654328541,
       roleAccept: ["พุทธรรม"],
       image: "https://image.makewebeasy.net/makeweb/0/wdU8ZjHiP/PicAngthong62/%E0%B8%97%E0%B8%AD%E0%B8%94%E0%B8%9C%E0%B9%89%E0%B8%B2%E0%B8%9B%E0%B9%88%E0%B8%B2_%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B8%95%E0%B9%89%E0%B8%99%E0%B8%97%E0%B8%AD%E0%B8%87_%E0%B8%88_%E0%B8%AD%E0%B9%88%E0%B8%B2%E0%B8%87%E0%B8%97%E0%B8%AD%E0%B8%87_11052019_%E0%B9%91%E0%B9%99%E0%B9%90%E0%B9%95%E0%B9%91%E0%B9%93_0286.jpg",
-      eventId: String,
-      clubName: String,
+      eventId: 12345,
+      clubName: "พุทธรรม",
     }
   ];
+  convertDatas = (data: Time[]): any => {
+    return data.map((item) => {
+      const dateS = item.startDate * 1000;
+      return {
+        eventHeader: item.eventHeader,
+        description: item.description,
+        attendees: item.attendees,
+        eventType: item.eventType,
+        location: item.location,
+        startDate: dayjs(item.startDate * 1000).locale("th").format('dd D MMM'),
+        endDate: dayjs(item.endDate * 1000).locale("th").format('dd D MMM'),
+        startTime: dayjs(item.startTime * 1000).locale("th").format('H:mm'),
+        endTime: dayjs(item.endTime * 1000).locale("th").format('H:mm'),
+        roleAccept: item.roleAccept,
+        image: item.image,
+        eventId: item.eventId,
+        clubName: item.eventId,
+      };
+    });
+  };
+  datas = this.convertDatas(this.dataBase);
+   
+  
+  
   isVisible = false;
 
   constructor() {}
---
+++ kku-board-project-frontend/src/app/interfaces/time.ts
@@ -0,0 +1,15 @@
+export interface Time {
+    eventHeader: String,
+    description: String,
+    attendees: number,
+    eventType: String,
+    location: String,
+    startDate: number,
+    endDate: number,
+    startTime: number,
+    endTime: number,
+    roleAccept: String[],
+    image: String,
+    eventId: number,
+    clubName: String,
+  }
