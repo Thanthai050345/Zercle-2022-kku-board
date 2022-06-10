4185dd2ea2a55fe1fe181cb614c306de835931ea
phakaphat <phakaphat.t@kkumail.com>
2022-06-10 19:18:59 +0700

    create api countdown

--- kku-board-project-backend/functions/src/routes/index.ts
+++ kku-board-project-backend/functions/src/routes/index.ts
@@ -3,12 +3,14 @@ import events from "./events";
 import register from "./register";
 import users from "./users";
 import topEvents from "./topEvents";
+import countdown from "./countDown"
 const router = express.Router();
 
 router.use("/users", users);
 router.use("/register", register);
 router.use("/events", events);
 router.use("/topEvents", topEvents);
+router.use("/countdown", countdown);
 
 
 export default router;
