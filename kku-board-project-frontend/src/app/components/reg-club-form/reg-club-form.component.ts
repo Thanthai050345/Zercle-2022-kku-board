99321cbf2e5581ebecaf943f89a016b62a9c8f72
phakaphat <phakaphat.t@kkumail.com>
2022-06-19 18:24:55 +0700

    create form reg club form and validate

--- kku-board-project-frontend/src/app/components/reg-club-form/reg-club-form.component.ts
+++ kku-board-project-frontend/src/app/components/reg-club-form/reg-club-form.component.ts
@@ -1,51 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { NzUploadFile } from 'ng-zorro-antd/upload';
-
-const getBase64 = (file: File): Promise<string | ArrayBuffer | null> =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = (error) => reject(error);
-  });
+import { facultys } from 'src/assets/datas/facultys';
 @Component({
   selector: 'app-reg-club-form',
   templateUrl: './reg-club-form.component.html',
   styleUrls: ['./reg-club-form.component.css'],
 })
 export class RegClubFormComponent implements OnInit {
+  facultys = facultys;
+  majors: any = [];
+  isMatch: boolean = false;
   signUpClubForm = new FormGroup({
     clubName: new FormControl('', [Validators.required]),
     email: new FormControl('', [Validators.required,Validators.email]),
-    password: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    confirmPassword: new FormControl('', [Validators.required, Validators.minLength(6)]),
     faculty: new FormControl('', [Validators.required]),
     major: new FormControl('', [Validators.required]),
-    urlImage: new FormControl('', [Validators.required]),
+    // urlImage: new FormControl('', [Validators.required]),
   });
   onSubmit(): void {
-    console.log(this.signUpClubForm);
-    
+    const password = this.signUpClubForm.value.password;
+    const confirmPassword = this.signUpClubForm.value.confirmPassword;
+    if (password === confirmPassword) {
+      console.log(this.signUpClubForm.value);
+    } else {
+      this.isMatch = true;
+    }
   }
 
   constructor() {}
 
   ngOnInit(): void {}
-  previewImage: string | undefined = '';
-  previewVisible = false;
-  fileList: NzUploadFile[] = [
-    {
-      uid: '-1',
-      name: 'image.png',
-      status: 'done',
-      url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
-    },
-  ];
-  handlePreview = async (file: NzUploadFile): Promise<void> => {
-    if (!file.url && !file['preview']) {
-      file['preview'] = await getBase64(file.originFileObj!);
-    }
-    this.previewImage = file.url || file['preview'];
-    this.previewVisible = true;
-  };
+
+  onChange(event: any) {
+    // console.log(event);
+    const faculty = facultys.find(faculty => faculty.value == event)
+    this.majors = faculty?.majors;
+  }
+  
 }
