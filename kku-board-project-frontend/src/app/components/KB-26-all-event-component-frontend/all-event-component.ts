d415501f62e33c35061995b072a83e3d5eb2e9b7
amonrit <76424455+amonrit@users.noreply.github.com>
2022-06-21 21:07:02 +0700

    pull

--- kku-board-project-frontend/src/app/components/KB-26-all-event-component-frontend/all-event-component.ts
+++ kku-board-project-frontend/src/app/components/KB-26-all-event-component-frontend/all-event-component.ts
@@ -10,71 +10,30 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./all-event-component.css'],
 })
 export class AllEventComponent implements OnInit {
-<<<<<<< HEAD
 
-  // data:any[] =[];
-  @Input() dataBase: any[] = [];
+  data:any[] =[];
+  @Input() dataBase:any[] =[];
 
 
-<<<<<<< Updated upstream
-=======
-  @Input() role = '';
->>>>>>> a3150d0edf45ec47b2f1198fa2d6dadfec8fec28
-  dataBase = [
-    {
-      eventHeader: 'มข.เข้าร่วมพิธีทอดผ้าป่าสนับสนุนโครงการทุนเล่าเรียนหลวงฯ',
-      description:
-        'ในสมัยพุทธกาล เมื่อพระผู้มีพระภาคเจ้าประทับ ณ พระเชตวนาราม ซึ่งเป็นพระอารามที่อนาถบิณฑิกเศรษฐีได้สร้างถวายเป็นพุทธนิวาส ได้มีภิกษุ ๓๐ รูป ชาวเมืองปาฐา ซึ่งอยู่ด้านทิศตะวันตกในแคว้นโกศลเดินทางมาหมายจะเฝ้าพระพุทธองค์ที่เมืองสาวัตถี แต่มาไม่ทันเพราะใกล้ถึงวัน',
-      attendees: 99,
-      eventType: 'onsite',
-      location: 'ณ วัดธาตุ พระอารามหลวงขอนแก่น',
-      startDate: 1654275600,
-      endDate: 1654362000,
-      startTime: 1654327541,
-      endTime: 1654328541,
-      roleAccept: ['พุทธรรม'],
-      image:
-        'https://image.makewebeasy.net/makeweb/0/wdU8ZjHiP/PicAngthong62/%E0%B8%97%E0%B8%AD%E0%B8%94%E0%B8%9C%E0%B9%89%E0%B8%B2%E0%B8%9B%E0%B9%88%E0%B8%B2_%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B8%95%E0%B9%89%E0%B8%99%E0%B8%97%E0%B8%AD%E0%B8%87_%E0%B8%88_%E0%B8%AD%E0%B9%88%E0%B8%B2%E0%B8%87%E0%B8%97%E0%B8%AD%E0%B8%87_11052019_%E0%B9%91%E0%B9%99%E0%B9%90%E0%B9%95%E0%B9%91%E0%B9%93_0286.jpg',
-      eventId: 12345,
-<<<<<<< HEAD
-      clubName: "พุทธรรม",
-    }
-=======
-      clubName: 'พุทธรรม',
-    },
-    {
-      eventHeader: 'รับน้องใหม่2565',
-      description: 'ปฐมนิเทศนักศึกษาใหม่และพิธีบายศรีสู่ขวัญนักศึกษาใหม่',
-      attendees: 99,
-      eventType: 'onsite',
-      location: 'ณ หอกาญจนาภิเษก',
-      startDate: 1554275600,
-      endDate: 1654362000,
-      startTime: 1654327541,
-      endTime: 1654328541,
-      roleAccept: ['KKU59'],
-      image:
-        'https://campus.campus-star.com/app/uploads/2019/07/KKU01-768x511.jpg',
-      eventId: 123,
-      clubName: 'องค์การนักศึกษา',
-    },
-    {
-      eventHeader: 'โครงการ ZERCLE INCUBATION PROGRAM',
-      description: 'ปฐมนิเทศนักศึกษาใหม่และพิธีบายศรีสู่ขวัญนักศึกษาใหม่',
-      attendees: 99,
-      eventType: 'onsite',
-      location: 'sci-park ชั้น 1',
-      startDate: 1654220600,
-      endDate: 1654362000,
-      startTime: 1654327541,
-      endTime: 1654328541,
-      roleAccept: ['KKU59'],
-      image: 'https://www.bloggang.com/data/vinitsiri/picture/1331280812.jpg',
-      eventId: 456,
-      clubName: 'CoE EN',
-    },
->>>>>>> a3150d0edf45ec47b2f1198fa2d6dadfec8fec28
-  ];
+  // dataBase = [
+  //   {
+  //     eventHeader: 'มข.เข้าร่วมพิธีทอดผ้าป่าสนับสนุนโครงการทุนเล่าเรียนหลวงฯ',
+  //     description:
+  //       'ในสมัยพุทธกาล เมื่อพระผู้มีพระภาคเจ้าประทับ ณ พระเชตวนาราม ซึ่งเป็นพระอารามที่อนาถบิณฑิกเศรษฐีได้สร้างถวายเป็นพุทธนิวาส ได้มีภิกษุ ๓๐ รูป ชาวเมืองปาฐา ซึ่งอยู่ด้านทิศตะวันตกในแคว้นโกศลเดินทางมาหมายจะเฝ้าพระพุทธองค์ที่เมืองสาวัตถี แต่มาไม่ทันเพราะใกล้ถึงวัน',
+  //     attendees: 99,
+  //     eventType: 'onsite',
+  //     location: 'ณ วัดธาตุ พระอารามหลวงขอนแก่น',
+  //     startDate: 1654275600,
+  //     endDate: 1654362000,
+  //     startTime: 1654327541,
+  //     endTime: 1654328541,
+  //     roleAccept: ['พุทธรรม'],
+  //     image:
+  //       'https://image.makewebeasy.net/makeweb/0/wdU8ZjHiP/PicAngthong62/%E0%B8%97%E0%B8%AD%E0%B8%94%E0%B8%9C%E0%B9%89%E0%B8%B2%E0%B8%9B%E0%B9%88%E0%B8%B2_%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B8%95%E0%B9%89%E0%B8%99%E0%B8%97%E0%B8%AD%E0%B8%87_%E0%B8%88_%E0%B8%AD%E0%B9%88%E0%B8%B2%E0%B8%87%E0%B8%97%E0%B8%AD%E0%B8%87_11052019_%E0%B9%91%E0%B9%99%E0%B9%90%E0%B9%95%E0%B9%91%E0%B9%93_0286.jpg',
+  //     eventId: 12345,
+  //     clubName: "พุทธรรม",
+  //   }
+  // ];
   convertDatas = (data: Event[]): any => {
     return data.map((item) => {
       return {
@@ -118,68 +77,14 @@ export class AllEventComponent implements OnInit {
     eventId: 0,
     clubName: ''
   };
-=======
-  // dataBase = [
-  //   {
-  //     eventHeader: "มข.เข้าร่วมพิธีทอดผ้าป่าสนับสนุนโครงการทุนเล่าเรียนหลวงฯ",
-  //     description: "ในสมัยพุทธกาล เมื่อพระผู้มีพระภาคเจ้าประทับ ณ พระเชตวนาราม ซึ่งเป็นพระอารามที่อนาถบิณฑิกเศรษฐีได้สร้างถวายเป็นพุทธนิวาส ได้มีภิกษุ ๓๐ รูป ชาวเมืองปาฐา ซึ่งอยู่ด้านทิศตะวันตกในแคว้นโกศลเดินทางมาหมายจะเฝ้าพระพุทธองค์ที่เมืองสาวัตถี แต่มาไม่ทันเพราะใกล้ถึงวัน",
-  //     attendees: 99,
-  //     eventType: "onsite",
-  //     location: "ณ วัดธาตุ พระอารามหลวงขอนแก่น",
-  //     startDate: 1654275600,
-  //     endDate: 1654362000,
-  //     startTime: 1654327541,
-  //     endTime: 1654328541,
-  //     roleAccept: ["พุทธรรม"],
-  //     image: "https://image.makewebeasy.net/makeweb/0/wdU8ZjHiP/PicAngthong62/%E0%B8%97%E0%B8%AD%E0%B8%94%E0%B8%9C%E0%B9%89%E0%B8%B2%E0%B8%9B%E0%B9%88%E0%B8%B2_%E0%B8%A7%E0%B8%B1%E0%B8%94%E0%B8%95%E0%B9%89%E0%B8%99%E0%B8%97%E0%B8%AD%E0%B8%87_%E0%B8%88_%E0%B8%AD%E0%B9%88%E0%B8%B2%E0%B8%87%E0%B8%97%E0%B8%AD%E0%B8%87_11052019_%E0%B9%91%E0%B9%99%E0%B9%90%E0%B9%95%E0%B9%91%E0%B9%93_0286.jpg",
-  //     eventId: 12345,
-  //     clubName: "พุทธรรม",
-  //   }
-  // ];
-  // convertDatas = (data: Even[]): any => {
-  //   return data.map((item) => {
-  //     return {
-  //       eventHeader: item.eventHeader,
-  //       description: item.description,
-  //       attendees: item.attendees,
-  //       eventType: item.eventType,
-  //       location: item.location,
-  //       startDate: dayjs(item.startDate * 1000).locale("th").format('ddd D MMM YYYY'),
-  //       endDate: dayjs(item.endDate * 1000).locale("th").format('ddd D MMM YYYY'),
-  //       startTime: dayjs(item.startTime * 1000).locale("th").format('ddd D MMM YYYY'),
-  //       endTime: dayjs(item.endTime * 1000).locale("th").format('ddd D MMM YYYY'),
-  //       roleAccept: item.roleAccept,
-  //       image: item.image,
-  //       eventId: item.eventId,
-  //       clubName: item.eventId,
-  //     };
-  //   });
-  // };
-  // datas = this.convertDatas(this.dataBase);
-
->>>>>>> Stashed changes
   isVisible = false;
 
   constructor(private userService: UserService) {}
-<<<<<<< HEAD
-  ngOnInit(): void {
-    // this.userService.getAllUser().subscribe(res => {
-    //   this.data = res;
-    //   console.log(res);
-    //   console.log(this.data[0]);
-    // });
-    // console.log(this.inputFromParent);    
-  }
-
-  showModal(): void {
-    
-=======
   ngOnInit(): void {}
 
   showModal(data: Event): void {
     console.log(data);
     this.datamodal = data;
->>>>>>> a3150d0edf45ec47b2f1198fa2d6dadfec8fec28
     this.isVisible = true;
   }
 
@@ -194,16 +99,7 @@ export class AllEventComponent implements OnInit {
   }
   cancel(): void {}
 
-<<<<<<< HEAD
   confirm(): void {
 
-=======
-  confirm(): void {}
-
-  getAllUser() {
-    this.userService.getAllUser().subscribe((res) => {
-      console.log(res);
-    });
->>>>>>> a3150d0edf45ec47b2f1198fa2d6dadfec8fec28
   }
 }
