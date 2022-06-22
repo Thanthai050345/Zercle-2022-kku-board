dc17fa3b1f26973bcde9e7ab0191b35ec7e21264
phakaphat <phakaphat.t@kkumail.com>
2022-06-22 19:21:31 +0700

    add page event

--- kku-board-project-frontend/src/app/components/calendar/calendar.component.ts
+++ kku-board-project-frontend/src/app/components/calendar/calendar.component.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/angular';
-// import { Calendar } from '@fullcalendar/core';
+import { EventCalendar } from 'src/app/interfaces/calendar';
+import { EventService } from 'src/app/services/event.service';
 
 @Component({
   selector: 'app-calendar',
@@ -9,97 +9,26 @@ import { CalendarOptions } from '@fullcalendar/angular';
   styleUrls: ['./calendar.component.css'],
 })
 export class CalendarComponent implements OnInit {
-  Events: any[] = [
+  @Input() events: EventCalendar[] = [
     {
-      title: 'All Day Event',
-      start: '2022-06-01',
-    },
-    {
-      title: 'Long Event',
-      start: '2022-06-01',
-      end: '2022-06-10',
-      color: 'purple', // override!
-    },
-    {
-      groupId: '999',
-      title: 'Repeating Event',
-      start: '2022-06-09T16:00:00',
-    },
-    {
-      groupId: '999',
-      title: 'Repeating Event',
-      start: '2022-06-16T16:00:00',
-    },
-    {
-      title: 'Conference',
-      start: '2022-06-11',
-      end: '2022-06-13',
-      color: 'purple', // override!
-    },
-    {
-      title: 'Meeting',
-      start: '2022-06-12T10:30:00',
-      end: '2022-06-12T12:30:00',
-    },
-    {
-      title: 'Lunch',
-      start: '2022-06-12T12:00:00',
-    },
-    {
-      title: 'Meeting',
-      start: '2022-06-12T14:30:00',
-    },
-    {
-      title: 'Birthday Party',
-      start: '2022-06-13T07:00:00',
-    },
-    {
-      title: 'Click for Google',
-      url: 'http://google.com/',
-      start: '2022-06-28',
+      color: '#134133',
+      end: '2022-06-23',
+      start: '2022-06-23',
+      title: 'กิจกรรมปลูกต้นไม้'
     },
   ];
 
-  /*------------------------------------------
-  --------------------------------------------
-  Define calendarOptions
-  --------------------------------------------
-  --------------------------------------------*/
+  constructor(private eventService: EventService) {}
+
+  ngOnInit() {}
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     dateClick: this.handleDateClick.bind(this),
     locale: 'th',
-    events: this.Events,
+    events: this.events,
   };
 
-  /*------------------------------------------
-  --------------------------------------------
-  Define constructor
-  --------------------------------------------
-  --------------------------------------------*/
-  constructor(private httpClient: HttpClient) {}
-
-  /*------------------------------------------
-  --------------------------------------------
-  Define ngOnInit()
-  --------------------------------------------
-  --------------------------------------------*/
-  ngOnInit() {
-    // this.httpClient
-    //     .get('http://localhost:8001/events.php')
-    //     .subscribe((res: any) => {
-    //       this.Events = res;
-    //       this.calendarOptions.events = this.Events;
-    //     });
-  }
-
-  /*------------------------------------------
-  --------------------------------------------
-  Define handleDateClick()
-  --------------------------------------------
-  --------------------------------------------*/
   handleDateClick(arg: any) {
-    // alert('date click! ' + arg.dateStr)
     console.log(arg);
   }
 }
