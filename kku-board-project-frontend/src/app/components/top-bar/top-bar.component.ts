971753640ddbc9bdc686aae59fb03413b15f69f7
phakaphat <phakaphat.t@kkumail.com>
2022-06-24 14:31:30 +0700

    delete log in topbar

--- kku-board-project-frontend/src/app/components/top-bar/top-bar.component.ts
+++ kku-board-project-frontend/src/app/components/top-bar/top-bar.component.ts
@@ -62,18 +62,15 @@ export class TopBarComponent implements OnInit {
     if (this.authority === 'student') {
       this.userService.getUserById(this.userUid).subscribe((res) => {
         this.user = res;
-        // console.log(this.user);
       });
       this.eventService.getCountdownUserById(this.userUid).subscribe((res) => {
         this.countdown = res.filter(
           (element) => this.aDay > element.startDate - Date.now()
         );
         this.datas = this.convertDatas(this.countdown);
-        console.log(this.datas);
       });
       this.eventService.getEventByStudentUid(this.userUid).subscribe((res) => {
         this.eventTable = this.convertEventTableDatas(res);
-        // console.log(this.eventTable);
       });
     } else if (this.authority === 'clubAdmin') {
       this.userService.getClubById(this.userUid).subscribe((res) => {
